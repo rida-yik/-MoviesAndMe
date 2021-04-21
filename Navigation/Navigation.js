a6bff492bb3f504ec37ad8edc59e35ae7a83c3d9
NAJJARI RIDA <johndoe@example.com>
2021-04-21 01:51:13 +0000

    yes

---
+++ Navigation/Navigation.js
@@ -0,0 +1,76 @@
+import React from 'react'
+import { StyleSheet, Image } from 'react-native';
+import { createStackNavigator, } from 'react-navigation-stack'
+import { createAppContainer  } from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation-tabs'
+import FilmDetail from '../Components/FilmDetail'
+import Search from '../Components/Search'
+import Favorites from '../Components/Favorites'
+
+
+
+const SearchStackNavigator = createStackNavigator({
+  Search: {
+    screen: Search,
+    navigationOptions: {
+      title: 'Rechercher'
+    }
+  },
+  FilmDetail: {
+    screen: FilmDetail
+  }
+})
+
+const FavoritesStackNavigator = createStackNavigator({
+  Favorites: {
+    screen: Favorites,
+    navigationOptions: {
+      title: 'Favoris'
+    }
+  },
+  FilmDetail: {
+    screen: FilmDetail
+  }
+})
+
+const MoviesTabNavigator = createBottomTabNavigator(
+  {
+    Search: {
+      screen: SearchStackNavigator,
+      navigationOptions: {
+        tabBarIcon: () => {
+          return <Image
+            source={require('../Images/ic_search.png')}
+            style={styles.icon}/>
+        }
+      }
+    },
+    Favorites: {
+      screen: FavoritesStackNavigator,
+      navigationOptions: {
+        tabBarIcon: () => {
+          return <Image
+            source={require('../Images/ic_favorite.png')}
+            style={styles.icon}/>
+        }
+      }
+    }
+  },
+  {
+    tabBarOptions: {
+      activeBackgroundColor: '#DDDDDD',
+      inactiveBackgroundColor: '#FFFFFF',
+      showLabel: false,
+      showIcon: true
+    }
+  }
+)
+
+const styles = StyleSheet.create({
+  icon: {
+    width: 30,
+    height: 30
+  }
+})
+
+export default createAppContainer(MoviesTabNavigator)
